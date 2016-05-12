9f51fea7f6bfd8a3f89fd9f61d1264cf82fd85cf
Leo Yao <Leo Yao>
2016-05-11 20:17:54 -0700

    updated drawings

--- src/Damager.java
+++ src/Damager.java
@@ -1,8 +1,19 @@
+import java.awt.Color;
+import java.awt.Graphics;
 
 public class Damager extends Player {
 
 	public Damager(int x, int y) {
 		super(x, y);
+	}
+	
+	public void act(){
+		moveBy(1,1);
+	}
+	
+	public void draw(Graphics g){
+		g.setColor(Color.green);
+		g.fillOval(getX(), getY(), getWidth(), getHeight());
 	}
 
 }

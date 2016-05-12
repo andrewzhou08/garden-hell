d3e968c406a97e205c9f19b79ac508da1112a7e8
Leo Yao <Leo Yao>
2016-05-12 10:59:33 -0700

    collision

--- src/Actor.java
+++ src/Actor.java
@@ -11,6 +11,7 @@ public abstract class Actor implements Drawable {
 	private HitBox h;
 	private int width, height;
 	private int x, y;
+	private int hp;
 	
 	public Actor(int x, int y, int width, int height) {
 		h = new HitBox(x, y, width, height);
@@ -18,6 +19,7 @@ public abstract class Actor implements Drawable {
 		this.height = height;
 		this.x = x;
 		this.y = y;
+		hp = 100;
 	}
 	
 	public Actor(String sprite, int x, int y, int width, int height) {
@@ -49,6 +51,10 @@ public abstract class Actor implements Drawable {
 		return height;
 	}
 	
+	public int getHP(){
+		return hp;
+	}
+	
 	public void move(int newX, int newY) {
 		h.move(newX, newY);
 		x = newX;
@@ -61,8 +67,8 @@ public abstract class Actor implements Drawable {
 		y = y + movedY;
 	}
 	// 1 = up, 2 = down, 3 = left, 4 = right
-	public boolean willCollide(ArrayList<Actor> actors, int direction){
-		boolean out = false;
+	public Actor willCollide(ArrayList<Actor> actors, int direction){
+		Actor out = null;
 		Rectangle window = new Rectangle(0,0,Main.WINDOW_WIDTH-8,Main.WINDOW_HEIGHT-29);
 		if(direction == 1)
 			h.move(x, y-1);
@@ -76,16 +82,27 @@ public abstract class Actor implements Drawable {
 			Actor a  = actors.get(i);
 			if(this != a){
 				if(h.intersects(a.h))
-					out = true;
-				if(!window.contains(h.getRectangle())){
-					out = true;
-				}
+					out = a;
+//				if(!window.contains(h.getRectangle())){
+//					out = ;
+//				}
 			}	
 		}
 		h.move(x, y);
 		return out;
 		
 		
+	}
+	/**
+	 * 
+	 * @param amount amount of hp lost
+	 * @return true if this character is dead
+	 */
+	public boolean loseHP(int amount){
+		hp-=amount;
+		if(hp<0)
+			return true;
+		return false;
 	}
 	
 	public abstract void act(); 

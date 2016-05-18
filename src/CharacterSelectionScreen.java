a0cb1f290021750965415215b32c11204bdac6c0
andrewzhou08 <andrewzhou08@gmail.com>
2016-05-17 22:21:24 -0700

    Add character selection screen

--- src/CharacterSelectionScreen.java
+++ src/CharacterSelectionScreen.java
@@ -1,4 +1,90 @@
+import java.awt.Graphics;
+import java.awt.Image;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 
-public class CharacterSelectionScreen {
+import javax.swing.ImageIcon;
+import javax.swing.JPanel;
 
+public class CharacterSelectionScreen extends JPanel implements MouseListener {
+
+	private Image characters;
+	private Image player;
+	private Main main;
+	private int playerNumber;
+	
+	public CharacterSelectionScreen(Main main){
+		this.main = main;
+		addMouseListener(this);
+		characters = (new ImageIcon("assets/character-selection-screen.png")).getImage();
+		player = (new ImageIcon("assets/character-selection-p1.png")).getImage();
+		playerNumber = 1;
+	}
+
+	@Override
+	public void mouseClicked(MouseEvent e) {
+		if(!main.gameStarted()){
+			int x = e.getX();
+			int y = e.getY();
+			if(playerNumber == 1){
+				if(x >= 100 && x <= 420 && y >= 45 && y <= 325){
+					main.setPlayer(1, new Tank(1, 9, 0));
+					player = (new ImageIcon("assets/character-selection-p2.png")).getImage();
+					repaint();
+				}
+				else if(x >= 480 && x <= 800 && y >= 45 && y <= 325){
+					main.setPlayer(1, new Damager(1, 9, 0));
+					player = (new ImageIcon("assets/character-selection-p2.png")).getImage();
+					repaint();
+				}
+				else if(x >= 880 && x <= 1160 && y >= 45 && y <= 325){
+					main.setPlayer(1, new Builder(1, 9, 0));
+					player = (new ImageIcon("assets/character-selection-p2.png")).getImage();
+					repaint();
+				}
+				playerNumber++;
+			}
+			else if(playerNumber == 2){
+				if(x >= 100 && x <= 420 && y >= 45 && y <= 325){
+					main.setPlayer(2, new Tank(30, 9, 180));
+					player = (new ImageIcon("assets/character-selection-start.png")).getImage();
+					repaint();
+				}
+				else if(x >= 480 && x <= 800 && y >= 45 && y <= 325){
+					main.setPlayer(2, new Damager(30, 9, 180));
+					player = (new ImageIcon("assets/character-selection-start.png")).getImage();
+					repaint();
+				}
+				else if(x >= 880 && x <= 1160 && y >= 45 && y <= 325){
+					main.setPlayer(2, new Builder(30, 9, 180));
+					player = (new ImageIcon("assets/character-selection-start.png")).getImage();
+					repaint();
+				}
+				playerNumber++;
+			}
+			else{
+				if(x >= 912 && x <= 1112 && y >= 650 && y <= 700){
+					main.changePanel("2");
+				}
+			}
+		}
+	}
+
+	@Override
+	public void mousePressed(MouseEvent e) {}
+
+	@Override
+	public void mouseReleased(MouseEvent e) {}
+
+	@Override
+	public void mouseEntered(MouseEvent e) {}
+
+	@Override
+	public void mouseExited(MouseEvent e) {}
+	
+	public void paintComponent(Graphics g){
+		g.drawImage(characters, 0, 0, Main.WINDOW_WIDTH, Main.WINDOW_HEIGHT, null);
+		
+		g.drawImage(player, 620, 350, 530, 293, null);
+	}
 }
--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,7 @@
+import java.awt.CardLayout;
+
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 
 public class Main extends JFrame {
 	
@@ -8,8 +11,10 @@ public class Main extends JFrame {
 	public static final int GRID_ROWS = 18;
 	public static final int CELL_WIDTH = WINDOW_WIDTH / GRID_COLS;
 	public static final int CELL_HEIGHT= WINDOW_HEIGHT / GRID_ROWS;
-		
+	
+	private JPanel cardPanel;
 	private GamePanel game;
+	private CharacterSelectionScreen screen;
 	
 	/**
 	 * Initializes GUI window
@@ -18,15 +23,39 @@ public class Main extends JFrame {
 		setBounds(0, 0, WINDOW_WIDTH, WINDOW_HEIGHT);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 		setResizable(false);
+		
+	    cardPanel = new JPanel();
+	    CardLayout cl = new CardLayout();
+	    cardPanel.setLayout(cl);
+		
 		game = new GamePanel();
+		screen = new CharacterSelectionScreen(this);
+		addMouseListener(screen);
 		addKeyListener(game);
 		add(game);
+		
+		cardPanel.add(screen, "1");
+		cardPanel.add(game, "2");
+		
+		add(cardPanel);
 		setVisible(true);
-		game.loop();
 	}
 	
 	public static void main(String[] args) {
 		Main main = new Main();
 	}
 	
+	public void changePanel(String name) {
+		((CardLayout)cardPanel.getLayout()).show(cardPanel,name);
+		requestFocus();
+		game.startThread();
+	}
+	
+	public boolean gameStarted(){
+		return game.gameStarted();
+	}
+	
+	public void setPlayer(int playerNumber, Player player){
+		game.setPlayer(playerNumber, player);
+	}
 }
